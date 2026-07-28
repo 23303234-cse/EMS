e3c15440cb39f1124fae15db8a4c6a4a21433e37
23303234-cse <23303234@iunat.edu>
2026-07-28 20:50:01 +0600

    First Commit

--- obj/Debug/net10.0/EMS.AssemblyInfo.cs
+++ obj/Debug/net10.0/EMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4978da0bb069dd60642f5b94e075fbcb5bb8ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
